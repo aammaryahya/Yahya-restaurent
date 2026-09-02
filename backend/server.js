734aae9516c14dfc0b4ec49b30e82a8efddeff3e
aammaryahya <aammar.yahya20@gmail.com>
2026-09-02 20:55:07 -0300

    Remove duplicate server listening code
    Removed duplicate server listening code.

--- backend/server.js
+++ backend/server.js
@@ -59,10 +59,3 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = io;
-
-
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-module.exports = io;
