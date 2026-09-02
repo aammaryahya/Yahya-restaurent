1a69cb4e9160b161dbcf94c7743f9e213328a83b
aammaryahya <aammar.yahya20@gmail.com>
2026-09-02 18:43:40 -0300

    Remove role restriction from register route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,7 +5,7 @@ const { register, login, profile, deleteUser,
 const auth = require('../middleware/auth');
 const role = require('../middleware/role');
 
-router.post('/register', auth, role(["admin"]), register);
+router.post('/register', register);
 router.post('/login', login);
 router.get('/profile', auth, profile);
 router.delete("/:id", auth, role(['admin']), deleteUser);
